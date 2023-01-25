31457f3489c5da952a8ba0fe00acf2b54c4a0aea
Alex Newman <alex.newman.prog@gmail.com>
2023-01-25 18:42:50 +0600

    Добавьте файлы проекта.

---
+++ SimpleHttpServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleHttpServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
