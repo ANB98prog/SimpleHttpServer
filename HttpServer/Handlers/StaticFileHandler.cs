13baa65870bd69f569c297226ddeec55d05f9576
Alex Newman <alex.newman.prog@gmail.com>
2023-01-27 18:39:42 +0600

    Implemented sync handle method

--- HttpServer/Handlers/StaticFileHandler.cs
+++ HttpServer/Handlers/StaticFileHandler.cs
@@ -1,4 +1,4 @@
-﻿namespace HttpServer
+﻿namespace HttpServer.Handlers
 {
     public class StaticFileHandler : IHandler
     {
@@ -10,7 +10,28 @@
         }
         public void Handle(Stream stream, Request request)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"Try to get file: '{request.url}'");
+
+            var fullPath = Path.Combine(_staticFilesPath,
+                                    string.Join(Path.DirectorySeparatorChar,
+                                        request.url.Split('/', StringSplitOptions.RemoveEmptyEntries)));
+
+            if (!File.Exists(fullPath))
+            {
+                Console.WriteLine("File not found!");
+                ResponseWriter.WriteResponseStatus(System.Net.HttpStatusCode.NotFound, stream);
+                using var writer = new StreamWriter(stream);
+                writer.WriteLine($"File {Path.GetFileName(fullPath)} is not found!");
+            }
+            else
+            {
+                Console.WriteLine("File successfully got.");
+                ResponseWriter.WriteResponseStatus(System.Net.HttpStatusCode.OK, stream);
+                using (var fileStream = File.OpenRead(fullPath))
+                {
+                    fileStream.CopyTo(stream);
+                }
+            }
         }
 
         public async Task HandleAsync(Stream stream, Request request)
@@ -18,7 +39,7 @@
             Console.WriteLine($"Try to get file: '{request.url}'");
 
             var fullPath = Path.Combine(_staticFilesPath,
-                                    String.Join(Path.DirectorySeparatorChar,
+                                    string.Join(Path.DirectorySeparatorChar,
                                         request.url.Split('/', StringSplitOptions.RemoveEmptyEntries)));
 
             if (!File.Exists(fullPath))
